6b572353f0ee774ce96aae5d780a8c3ec423a1d8
Anton_Dyrdin <a.dyrdin@oblakogroup.ru>
2024-06-03 04:29:47 +0300

    wip

--- trader.py
+++ trader.py
@@ -8,38 +8,23 @@ class Trader:
         self.ema_alfa1 = hyperparameters.get('ema_alfa1', 0.1)
         self.ema_alfa2 = hyperparameters.get('ema_alfa2', 0.1)
         self.ema_diff_buy = hyperparameters.get('ema_diff_buy', 0.1)
-        self.take_profit = hyperparameters.get('take_profit', 0.8)
+        self.take_profit = hyperparameters.get('take_profit', 1.0)
+        self.trade_amount = hyperparameters.get('trade_amount', 1)
         self.exmo_bid_window = []
         self.moex_usdrub_tod_window = []
         self.tick_count = 0
-        self.positions = []  # Список для хранения позиций (покупок)
-        self.sell_orders = []  # Список для хранения ордеров на продажу
+        self.positions = []  # Список для хранения позиций (цены покупки)
 
     def get_profit(self, bid_usdtrub):
         return self.balance_rub + (self.balance_usdt * bid_usdtrub)
 
     def calculate_ema(self, prices, alpha=0.1):
-        if not prices:
-            return 0
         ema = [prices[0]]  # начальное значение EMA равно первому значению цены
         for price in prices[1:]:
             ema.append(ema[-1] + alpha * (price - ema[-1]))
         return ema[-1]
 
-    def place_buy_order(self, exmo_ask):
-        amount_to_trade = self.hyperparameters.get('trade_amount', 1)
-        if self.balance_rub >= amount_to_trade * exmo_ask:
-            self.balance_rub -= amount_to_trade * exmo_ask
-            self.balance_usdt += amount_to_trade
-            self.positions.append((amount_to_trade, exmo_ask))
-            print(f"Buy order placed: {amount_to_trade} USDT at {exmo_ask} RUB")
-
-    def place_sell_order(self, amount, buy_price):
-        sell_price = buy_price + self.take_profit
-        self.sell_orders.append((amount, sell_price))
-        print(f"Sell order placed: {amount} USDT at {sell_price} RUB")
-
-    def process_tick(self, exmo_bid, exmo_ask, moex_usdrub_tod):
+    def process_tick(self, exmo_bid, exmo_ask, moex_usdrub_tod, moex_open):
         self.exmo_bid_window.append(exmo_bid)
         self.moex_usdrub_tod_window.append(moex_usdrub_tod)
         self.tick_count += 1
@@ -52,58 +37,42 @@ class Trader:
 
         # Если накоплено недостаточно данных, просто выходим
         if self.tick_count <= self.window_size + self.ignore_last:
-            return
+            return None
+        
 
-        # Рассчет EMA с игнорированием последних L значений
         ema_exmo_bid = self.calculate_ema(self.exmo_bid_window[:-self.ignore_last], self.ema_alfa1)
         ema_moex_usdrub = self.calculate_ema(self.moex_usdrub_tod_window[:-self.ignore_last], self.ema_alfa2)
-        
-        # Медленно меняющаяся разница между биржами
         ema_diff = ema_moex_usdrub - ema_exmo_bid
+        indicator = moex_usdrub_tod - exmo_bid - ema_diff
 
-        # Если цена на EXMO после корректировки на "постоянную" разницу ушла ниже, чем ema_diff_buy
-        if ema_diff - exmo_bid > self.ema_diff_buy:
-            # Покупка
-            self.place_buy_order(exmo_ask)
+        if not moex_open:
+            # Пропускаем, если MOEX не работает
+            return [], ema_diff, indicator
 
-        # Проверка на исполнение ордеров на продажу
-        for order in self.sell_orders.copy():
-            amount, sell_price = order
+        # Логика торговли
+        trades = []
+        if indicator > self.ema_diff_buy:
+            self.balance_rub -= self.trade_amount * exmo_ask
+            self.balance_usdt += self.trade_amount
+            self.positions.append(exmo_ask)
+            trades.append(('buy', exmo_ask))
+
+            # Выставление ордера на продажу
+            sell_price = exmo_ask + self.take_profit
+            trades.append(('sell_order', sell_price))
+
+        # Проверка ордеров на продажу
+        positions_to_remove = []
+        for buy_price in self.positions:
+            sell_price = buy_price + self.take_profit
             if exmo_bid >= sell_price:
-                self.balance_rub += amount * exmo_bid
-                self.balance_usdt -= amount
-                self.sell_orders.remove(order)
-                print(f"Sell order executed: {amount} USDT at {sell_price} RUB")
+                self.balance_rub += self.trade_amount * exmo_bid
+                self.balance_usdt -= self.trade_amount
+                trades.append(('sell', exmo_bid))
+                positions_to_remove.append(buy_price)
 
-        # Установка ордеров на продажу для новых покупок
-        while self.positions:
-            amount, buy_price = self.positions.pop(0)
-            self.place_sell_order(amount, buy_price)
+        # Удаление реализованных позиций
+        for position in positions_to_remove:
+            self.positions.remove(position)
 
-
-# Пример использования
-hyperparameters = {'trade_amount': 10,
-                   'window_size': 5,
-                   'ignore_last': 2,
-                   'ema_alfa1': 0.1,
-                   'ema_alfa2': 0.1,
-                   'ema_diff_buy': 0.1, 
-                   'take_profit': 0.8}
-
-trader = Trader(balance_rub=1000, balance_usdt=0, hyperparameters=hyperparameters)
-
-# Пример тиков
-ticks = [
-    (76, 74, 75), (77, 75, 74), (78, 76, 73),
-    (75, 73, 74), (74, 72, 73), (73, 71, 72)
-]
-
-for exmo_bid, exmo_ask, moex_usdrub_tod in ticks:
-    trader.process_tick(exmo_bid, exmo_ask, moex_usdrub_tod)
-
-# Обновленный баланс и профит после обработки тиков
-print(f'Updated balance RUB: {trader.balance_rub}')
-print(f'Updated balance USDT: {trader.balance_usdt}')
-bid_usdtrub = 75
-profit = trader.get_profit(bid_usdtrub)
-print(f'Updated profit: {profit}')
+        return trades, ema_diff, indicator
--- trading_test.py
+++ trading_test.py
@@ -1,6 +1,7 @@
 import pandas as pd
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui, QtWidgets
+import time
 
 from trader import Trader
 
@@ -12,7 +13,7 @@ class TradingTest:
         self.exmo_data = None
         self.moex_data = None
         self.ema_diffs = []
-        self.trades = []  # Список для хранения точек сделок
+        self.trades = []
         self.data = None
 
     # Метод для загрузки данных с заданной стартовой даты
@@ -26,74 +27,124 @@ class TradingTest:
         self.moex_data = pd.read_csv(self.moex_file, delimiter=';', dtype={'<DATE>': str, '<TIME>': str})
         self.moex_data['datetime'] = pd.to_datetime(self.moex_data['<DATE>'] + ' ' + self.moex_data['<TIME>'], format='%d%m%y %H%M%S')
         self.moex_data.set_index('datetime', inplace=True)
-
+        
+        # Удаление дубликатов по индексу
+        self.moex_data = self.moex_data[~self.moex_data.index.duplicated(keep='first')]
+        # Клонирование DataFrame до ресемплинга
+        moex_data_clone = self.moex_data.copy()
+        
+        # Ресемплинг данных с шагом в 1 минуту и заполнение пропусков последними известными значениями
+        self.moex_data = self.moex_data.resample('1min').ffill()
+        
+        # Создание столбца "moex_open" и заполнение его на основе клона
+        self.moex_data['moex_open'] = self.moex_data.index.isin(moex_data_clone.index)
+    
         # Применение фильтра по стартовой дате
         start_datetime = pd.to_datetime(start_date, format='%d.%m.%Y')
         self.exmo_data = self.exmo_data[self.exmo_data.index >= start_datetime]
         self.moex_data = self.moex_data[self.moex_data.index >= start_datetime]
 
-        # Синхронизация данных
+        # Объединение данных с использованием merge_asof
         self.data = pd.merge_asof(self.exmo_data, self.moex_data, on='datetime', direction='forward')
-
+        self.data.to_csv('moex_data_filled.csv', sep=';')
+        
     def run_backtest(self):
-        for idx, row in self.data.iterrows():
-            if pd.isna(row['<CLOSE>_y']):
-                continue  # Пропускаем, если MOEX не работает
+        app = QtWidgets.QApplication([])
 
+        win = pg.GraphicsLayoutWidget(show=True, title="Trading Backtest")
+        win.showMaximized()
+        win.setWindowTitle('Trading Backtest with PyQtGraph')
+
+        pg.setConfigOptions(antialias=True)
+        
+        # Создание осей для отображения времени
+        axis1 = pg.graphicsItems.DateAxisItem.DateAxisItem(orientation='bottom')
+        axis2 = pg.graphicsItems.DateAxisItem.DateAxisItem(orientation='bottom')
+        axis3 = pg.graphicsItems.DateAxisItem.DateAxisItem(orientation='bottom')
+        axis4 = pg.graphicsItems.DateAxisItem.DateAxisItem(orientation='bottom')
+
+        # Создание графиков
+        # indicator_plot = win.addPlot(title="Indicator", axisItems={'bottom': axis1})
+        # win.nextRow()
+        # plot1 = win.addPlot(title="EMA Diff", axisItems={'bottom': axis2})
+        # win.nextRow()
+        plot2 = win.addPlot(title="Profit", axisItems={'bottom': axis3})
+        win.nextRow()
+        plot3 = win.addPlot(title="Prices", axisItems={'bottom': axis4})
+        
+        # Связка осей для синхронной прокрутки
+        # indicator_plot.setXLink(plot3)
+        plot2.setXLink(plot3)
+        # plot1.setXLink(plot3)
+
+        # indicator_curve = indicator_plot.plot(pen='cyan')
+        # ema_diff_curve = plot1.plot(pen='y')
+        profit_curve = plot2.plot(pen='g')
+        exmo_bid_curve = plot3.plot(pen='r')
+        exmo_ask_curve = plot3.plot(pen='b')
+        moex_usdrub_tod_curve = plot3.plot(pen='g')
+        buy_scatter = pg.ScatterPlotItem(symbol='o', size=10, pen=pg.mkPen(None), brush=pg.mkBrush(0, 255, 0, 255))
+        sell_scatter = pg.ScatterPlotItem(symbol='x', size=10, pen=pg.mkPen(None), brush=pg.mkBrush(255, 50, 50, 255))
+
+        plot3.addItem(buy_scatter)
+        plot3.addItem(sell_scatter)
+        
+        profit_series = []
+        indicator_series = []
+        exmo_bid_series = []
+        exmo_ask_series = []
+        moex_series = []
+
+        for idx, row in self.data.iterrows():
+            timestamp = row['datetime'].timestamp()  # Преобразование индекса в timestamp
             exmo_bid = row['<CLOSE>_x']
             exmo_ask = row['<CLOSE>_x'] + 0.60
             moex_usdrub_tod = row['<CLOSE>_y']
 
-            self.trader.process_tick(exmo_bid, exmo_ask, moex_usdrub_tod)
+            # почему-то если торговать при закрытой MOEX, то прибиль получается больше
+            result = self.trader.process_tick(exmo_bid, exmo_ask, moex_usdrub_tod, True)
+            # result = self.trader.process_tick(exmo_bid, exmo_ask, moex_usdrub_tod, row['moex_open'])
+            if result is None:
+                continue
+            
+            trades, ema_diff, indicator = result
+            self.ema_diffs.append((timestamp, ema_diff))
+            profit_series.append((timestamp, self.trader.get_profit(exmo_bid)))
+            indicator_series.append((timestamp, indicator))
+            exmo_bid_series.append((timestamp, exmo_bid))
+            exmo_ask_series.append((timestamp, exmo_ask))
+            moex_series.append((timestamp, moex_usdrub_tod))
 
-            if self.trader.tick_count > self.trader.window_size + self.trader.ignore_last:
-                ema_exmo_bid = self.trader.calculate_ema(self.trader.exmo_bid_window[:-self.trader.ignore_last], self.trader.ema_alfa1)
-                ema_moex_usdrub = self.trader.calculate_ema(self.trader.moex_usdrub_tod_window[:-self.trader.ignore_last], self.trader.ema_alfa2)
-                ema_diff = ema_moex_usdrub - ema_exmo_bid
-                self.ema_diffs.append((idx, ema_diff))
+            for trade_type, price in trades:
+                if trade_type == 'buy':
+                    self.trades.append((timestamp, 'buy', price))
+                elif trade_type == 'sell':
+                    self.trades.append((timestamp, 'sell', price))
 
-                # Логика торговли
-                if ema_diff - exmo_bid > self.trader.ema_diff_buy:
-                    self.trades.append((idx, 'buy', exmo_ask))
-                elif ema_moex_usdrub > exmo_ask:
-                    self.trades.append((idx, 'sell', exmo_bid))
+        # ema_diff_curve.setData([x for x, y in self.ema_diffs], [y for x, y in self.ema_diffs])
+        # indicator_curve.setData([x for x, y in indicator_series], [y for x, y in indicator_series])
+        profit_curve.setData([x for x, y in profit_series], [y for x, y in profit_series])
+        exmo_bid_curve.setData([x for x, y in exmo_bid_series], [y for x, y in exmo_bid_series])
+        exmo_ask_curve.setData([x for x, y in exmo_ask_series], [y for x, y in exmo_ask_series])
+        moex_usdrub_tod_curve.setData([x for x, y in moex_series], [y for x, y in moex_series])
+        
+        buy_scatter.setData([x for x, trade_type, y in self.trades if trade_type == 'buy'], [y for x, trade_type, y in self.trades if trade_type == 'buy'])
+        sell_scatter.setData([x for x, trade_type, y in self.trades if trade_type == 'sell'], [y for x, trade_type, y in self.trades if trade_type == 'sell'])
+        QtWidgets.QApplication.processEvents()
 
-    def plot_results(self):
-        fig = make_subplots(rows=3, cols=1, shared_xaxes=True, subplot_titles=("EMA Diff", "Profit", "Prices"))
+        QtWidgets.QApplication.exec_()
 
-        ema_diff_series = pd.Series({x: y for x, y in self.ema_diffs})
-        profit_series = pd.Series({x: self.trader.get_profit(row['<CLOSE>_x']) for x, row in self.data.iterrows() if row.name <= row.name})
-        exmo_bid_series = self.data['<CLOSE>_x']
-        exmo_ask_series = self.data['<CLOSE>_x'] + 0.60
-        moex_usdrub_tod_series = self.data['<CLOSE>_y']
-
-        fig.add_trace(go.Scatter(x=ema_diff_series.index, y=ema_diff_series.values, mode='lines', name='EMA Diff'), row=1, col=1)
-        fig.add_trace(go.Scatter(x=profit_series.index, y=profit_series.values, mode='lines', name='Profit'), row=2, col=1)
-        fig.add_trace(go.Scatter(x=exmo_bid_series.index, y=exmo_bid_series.values, mode='lines', name='Exmo Bid'), row=3, col=1)
-        fig.add_trace(go.Scatter(x=exmo_ask_series.index, y=exmo_ask_series.values, mode='lines', name='Exmo Ask'), row=3, col=1)
-        fig.add_trace(go.Scatter(x=moex_usdrub_tod_series.index, y=moex_usdrub_tod_series.values, mode='lines', name='MOEX USD/RUB TOD'), row=3, col=1)
-
-        for idx, trade_type, price in self.trades:
-            color = 'green' if trade_type == 'buy' else 'red'
-            fig.add_trace(go.Scatter(x=[idx], y=[price], mode='markers', marker=dict(color=color, size=10), name=trade_type.capitalize()), row=3, col=1)
-
-        fig.update_layout(height=900, width=1200, title_text="Trading Backtest Results")
-        fig.show()
-
-
-# Пример использования
 hyperparameters = {
-    'window_size': 10,
-    'ignore_last': 2,
+    'window_size': 40,
+    'ignore_last': 1,
     'ema_alfa1': 0.1,
     'ema_alfa2': 0.1,
-    'ema_diff_buy': 0.1,
-    'take_profit': 0.8,
+    'ema_diff_buy': 0.9,
+    'take_profit': 0.3,
     'trade_amount': 1
 }
 
 trader = Trader(balance_rub=1000, balance_usdt=0, hyperparameters=hyperparameters)
 test = TradingTest(trader, 'exmo_USDT_RUB_2024.csv', 'mmvb_USDRUB_TOD_2024.csv')
-test.load_data('21.05.2024')
+test.load_data('22.03.2024')
 test.run_backtest()
-test.plot_results()
